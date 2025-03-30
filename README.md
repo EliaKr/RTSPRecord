7e656f0cc5a7c284b11720d70439af2310b0a8b0
EliaKr <ikrigos@gmail.com>
2025-03-30 13:44:19 +0300

    Initial version

--- README.md
+++ README.md
@@ -1,4 +1,52 @@
 # RTSPRecord
+
 A simple RTSP stream recorder that records a stream in segments and supports looping the recording once storage space runs out. Ideal for use in low-powered devices like the Raspberry Pi.
 
-#### Note: Unencrypted RTSP streams can be easily intercepted. If you want to record a device outside of your local network the best solution is a site-to-site VPN. 
+## Dependencies
+
+- ffmpeg
+
+## Usage
+
+### Manual Use
+
+1. Clone this repository
+2. Add the stream information to the streamlist.json file according to the example. You can add more entries to add more streams to be recorded. Make sure the record directories are accessible by the program.
+3. Run RTSPRecord.py
+
+### Use with a system service
+
+1. Clone this repository
+2. Add the stream information to the streamlist.json file according to the example. You can add more entries to add more streams to be recorded. Make sure the record directories are accessible by the program.
+3. Make RTSPRecord.py executable
+` chmod +x /path/to/script/directory/RTSPStream.py `
+4. Create a new service
+`sudo nano /etc/systemd/system/RTSPRecord.service`
+
+Add the following content to the service file.
+
+```bash
+[Unit]
+Description=RTSPRecord Service
+After=network.target
+
+[Service]
+ExecStart=/usr/bin/python3 /path/to/script/directory/RTSPStream.py
+WorkingDirectory=/path/to/script/directory
+Restart=always
+User=youruser
+Group=yourgroup
+
+[Install]
+WantedBy=multi-user.target
+```
+
+5. Reload systemd and enable the service
+
+```bash
+sudo systemctl daemon-reload
+sudo systemctl enable RTSPRecord.service
+sudo systemctl start RTSPRecord.service
+```
+
+#### Note: Unencrypted RTSP streams can be easily intercepted. If you want to record a device outside of your local network the best solution is a site-to-site VPN.
---
+++ RTSPRecord.py
@@ -0,0 +1,125 @@
+from datetime import datetime
+import json
+import time
+import subprocess
+import os
+import multiprocessing
+
+class Config:
+    def __init__(self):
+        pass
+    
+class StreamList:
+    def __init__(self):
+        self.streamList = self.loadStreamList()
+    
+    def loadStreamList(self):
+        streamList = []
+        with open("streamlist.json", "r") as streamListFile:
+            streamListDictionary = json.load(streamListFile)
+            for streamNameKey in streamListDictionary.keys():
+                streamObject = self.createStreamObject(streamNameKey, streamListDictionary)
+                streamList.add(streamObject)
+        return streamList
+
+    def createStreamObject(self, streamNameKey, streamListDictionary):
+        tempStreamObject = Stream()
+        tempStreamObject.setInfo(streamNameKey, streamListDictionary[streamNameKey]['address'], streamListDictionary[streamNameKey]['port'], streamListDictionary[streamNameKey]['username'], streamListDictionary[streamNameKey]['password'], streamListDictionary[streamNameKey]['streamPath'], streamListDictionary[streamNameKey]['recordPath'], streamListDictionary[streamNameKey]['recordingSegmentDuration'])
+        return tempStreamObject
+    
+    def getStreamList(self):
+        return self.streamList
+
+class Stream:
+    def __init__(self):
+        pass
+    
+    def setInfo(self, streamName, streamAddress, streamPort, streamUsername, streamPassword, streamPath, recordPath, recordingSegmentDuration):
+        self.streamName = streamName
+        self.streamAddress = streamAddress
+        self.streamPort = streamPort
+        self.streamUsername = streamUsername
+        self.streamPassword = streamPassword
+        self.streamPath = streamPath
+        self.recordPath = StorageLocation(recordPath)
+        self.recordingSegmentDuration = recordingSegmentDuration
+        self.rtspUrl = self.createRtspUrl()
+
+    def createRtspUrl(self):
+        rtspUrl = "rtsp://" + self.streamUsername + ":" + self.streamPassword + "@" + self.streamAddress + ":" + self.streamPort + self.streamPath
+        return rtspUrl
+    
+class Recording:
+    def __init__(self):
+        pass
+    
+    def recordAllStreams(self, streamList):
+        for i in streamList:
+            self.startRecordingProcess(i)
+
+    def startRecordingProcess(self, streamObject):
+        newProcess = multiprocessing.Process(self.startLoopedRecording, streamObject)
+        newProcess.start()
+        print(f"Started process for {streamObject.streamName} with ID: {newProcess.pid}")
+
+    def startLoopedRecording(self, streamObject):
+        while True:
+            while streamObject.recordPath.getDirectorySize() < streamObject.recordPath.maxSize:
+                current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+                recording_filename = f"{streamObject.recordPath.directory}_{current_time}.mp4"
+                recording_path = streamObject.recordPath.directory + "/" + recording_filename
+
+                ffmpeg_process = subprocess.Popen(f"ffmpeg -rtsp_transport tcp -i {streamObject.rtspUrl} -c:v copy -c:a copy -f mp4 {recording_path}", shell=True)
+                time.sleep(streamObject.recordingSegmentDuration)
+                ffmpeg_process.terminate()
+            else:
+                streamObject.recordPath.deleteOldestFile()
+            
+class StorageLocation:
+    def __init__(self, generalDirectory):
+        self.generalDirectory = generalDirectory
+        self.setMaxSize()
+
+    def getDirectorySize(self):
+        directorySize = 0
+        for dirpath, dirnames, filenames in os.walk(self.directoryPath):
+            for f in filenames:
+                fp = os.path.join(dirpath, f)
+                directorySize += os.path.getsize(fp)
+        return directorySize
+
+    def createDirectory(self, streamName):
+        self.directoryPath = os.path.join(self.generalDirectory, streamName)
+        if not os.path.exists(self.directoryPath):
+            os.makedirs(self.directoryPath)
+
+
+    def setMaxSize(self):
+        with open('/home/elias/Documents/repos/RTSPRecord/config.txt', 'r') as file:
+            for line in file:
+                if line.startswith('directoryMaxSize'):
+                    self.maxSize = int(line.split('=')[1].strip())
+                    break
+    
+    def deleteOldestFile(self):
+        files = os.listdir(self.directory)
+        oldest_file = min(files, key=lambda f: datetime.strptime(f.split(".")[0], "%Y-%m-%d_%H-%M-%S"))
+        file_path = os.path.join(self.directory, oldest_file)
+        os.remove(file_path)
+
+class Main:
+    def __init__(self):
+        self.setup()
+        self.run()
+
+    def setup(self):
+        self.streamList = StreamList()
+    
+    def run(self):
+        recordObject = Recording()
+        recordObject.recordAllStreams(self.streamList)
+
+        while True:
+            pass
+
+run = Main()
